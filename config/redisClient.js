4acf31b1e5d6e7a2295fda287f90bb107760d498
yashkurechiya <2005yashkurechiya@gmail.com>
2026-01-20 16:23:39 +0530

    redis

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,7 +1,7 @@
 // redisClient.js
 import { createClient } from "redis";
 
-const redis = createClient({ url: "redis://localhost:6379" });
+const redis = createClient({ url:  process.env.REDIS_URL });
 redis.connect();
 
 export default redis;
