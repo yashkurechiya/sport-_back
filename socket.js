46e669541916ccd4630f8e447aea0d2765a5f43f
yashkurechiya <2005yashkurechiya@gmail.com>
2026-01-21 14:25:08 +0530

    socket connected

--- socket.js
+++ socket.js
@@ -2,7 +2,19 @@
 import { Server } from "socket.io";
 
 export const initSocket = (server) => {
-  const io = new Server(server, { cors: { origin: "http://localhot:3000" } });
+
+  const io = new Server(server, {
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "https://sport-front-sooty.vercel.app",
+        "https://www.goindia.online"
+      ],
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
 
   io.on("connection", (socket) => {
     socket.on("joinTournament", (id) => {
@@ -13,7 +25,7 @@ export const initSocket = (server) => {
       io.to(`tournament_${data.tournamentId}`).emit("receiveMessage", data);
     });
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => { });
   });
 
   return io;
